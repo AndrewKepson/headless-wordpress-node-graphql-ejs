46022c4ca92110e8292a0550d9bef4d89ad5b476
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2022-01-25 18:04:59 -0700

    Other fix

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import postRoutes from './routes/postRoutes.js'
 
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
 app
   .use(express.urlencoded({ extended: true })) // Handle request parameters
   .use(express.json()) // Handle request body
@@ -13,4 +15,4 @@ app
   .use('/blog', postRoutes) // Blog routes
   .get('/', (req, res) => res.render('index')) // Static pages routing
   .get('/about', (req, res) => res.render('about'))
-  .listen(3000, () => console.log(`App is listening on port 3000`))
+  .listen(PORT, () => console.log(`App is listening on port ${PORT}`))
